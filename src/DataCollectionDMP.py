692acab7e3520e106f68b2c7d08ea6b1bd9fd223
Dylan Miller <djm2@ualberta.ca>
2024-04-17 15:24:10 -0600

    WIP: save data to HDF5 file

--- src/DataCollectionDMP.py
+++ src/DataCollectionDMP.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python3
 
+import sys
+import threading
 import numpy as np
 import rospy 
 import h5py
 import time
-import numpy
+import cv2
 
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
-from std_msgs.msg import String
 from wam_srvs.srv import JointMove
-from wam_srvs.srv import Hold
 from std_srvs.srv import Empty
 
-import json
-import glob
+class VideoCatureThread(threading.Thread):
+    def __init__(self, callback):
+        threading.Thread.__init__(self)
+        self.callback = callback
+        self.running = False
 
-def joint_move(pos_goal: np.ndarray):
-        """Move WAM to a desired position.
-        q is a numpy array of length 7 that specifies the joint angles
-        """
-  # Communicate with /wam/joint_move service on control computer
-        rospy.wait_for_service('/wam/joint_move')
-        try:
-            #print('found service')
-            joint_move_service = rospy.ServiceProxy('/wam/joint_move', JointMove)
-            joint_move_service(pos_goal)
-            #print('called move_q')
-        except rospy.ServiceException as e:
-            print("Service call failed: %s" % e)
+    def run(self):
+        self.running = True
+        # pipe_line = "aravissrc camera-name=FLIR-0119E8A8 ! video/x-raw, format=RGB ! videoconvert ! appsink"
+        pipe_line = "videotestsrc ! appsink"
+        cap = cv2.VideoCapture(pipe_line, cv2.CAP_GSTREAMER)
+        # if not cap.isOpened():
+        #     print("Error: Could not open video stream")
+        #     return
+        while self.running:
+            ret, frame = cap.read()
+            if ret:
+                self.callback(frame)
+            else:
+                print("Error: Could not read frame")
+                
+        cap.release()
 
+    def stop(self):
+        self.running = False
+            
 
 class DataRecorder(object):
 
     def __init__(self, save_file):
         rospy.Subscriber('/wam/joint_states', JointState, self.cb_joint_state)
         rospy.Subscriber('/wam/pose', PoseStamped, self.cb_ee_pose)
-        rospy.Subscriber('keyboard_command', String, self.cb_keyboard)
+        # rospy.Subscriber('keyboard_command', String, self.cb_keyboard)
         self.open_grasp = rospy.ServiceProxy('/zeus/bhand/open_grasp', Empty) 
         self.close_grasp = rospy.ServiceProxy('/zeus/bhand/close_grasp', Empty)
         self.save_file = save_file
@@ -45,70 +54,98 @@ class DataRecorder(object):
 
         self.joint_state_data = []
         self.ee_pose_data = []
+        self.joint_state_time = []
+        self.video = []
         self.collect = False
 
     def reset(self):
-        self.joint_state_data = [1]
-        self.ee_pose_data = [0]
+        self.joint_state_data = []
+        self.ee_pose_data = []
+        self.joint_state_time = []
         self.collect = False
 
     def get_next_group_id(self):
         existing_groups = self.hd5f_file.keys()
-        if len(existing_groups) == 0:
-            return 0
-        else:
-            return max([int(g) for g in existing_groups]) + 1
-    
+        group_len = len(existing_groups)
+        return group_len 
+
     def cb_joint_state(self, data : JointState):
-        joint_state = {'time': data.header.stamp.secs+data.header.stamp.nsecs*10^(-9),
-                'position' : data.position,
-                'velocity' : data.velocity,
-                'effort' : data.effort}
         if self.collect:
+            joint_state = np.array([data.position, data.velocity, data.effort], dtype=float)
+            self.joint_state_time.append(data.header.stamp.nsecs)
             self.joint_state_data.append(joint_state)
     
     def cb_ee_pose(self, data : PoseStamped):
-        EE_pose = {'position' : (data.pose.position.x, data.pose.position.y, data.pose.position.z),
-                'orientation': (data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z,  data.pose.orientation.w)}
         if self.collect:
-            self.ee_pose_data.append(EE_pose)
+            pose = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z, data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+            self.ee_pose_data.append(pose)
 
-    def cb_keyboard(self, key_pressed : String):
+    def cb_video_capture(self, data):
+        if self.collect:
+            self.video.append(data)
+
+    def cb_keyboard(self, key_pressed):
 
         # Begin data collection
-        if key_pressed.data == 'b':
-            print('Data collecting started')
+        if key_pressed == 'b':
+            print('\nData collecting started')
             self.collect = True
         
         # End data collection
-        elif key_pressed.data == 'e':
-            print('Data collecting ended')
+        elif key_pressed == 'e':
+            print('\nData collecting ended')
             group_id = self.get_next_group_id()
-            print('Group ID:', group_id)
-            traj_group = self.hd5f_file.create_group(str(group_id))
+            traj_group = self.hd5f_file.create_group(f"traj_{group_id}")
 
             traj_group.attrs['creation_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-            # traj_group.create_dataset('joint_state', data=self.joint_state_data)
-            # traj_group.create_dataset('joint_state', data=self.joint_state_data)
-            traj_group.create_dataset('joint_state', data=range(10))
-            traj_group.create_dataset('ee_pose', data=range(10))
+            joint_state_data = np.array(self.joint_state_data)
+            joint_dset = traj_group.create_dataset('joint_state', data=joint_state_data)
+            joint_dset.attrs['description'] = "size = (n,3,dof) where 3 is (position, velocity, effort)" 
+            pose_data = np.array(self.ee_pose_data)
+            pose_dset = traj_group.create_dataset('pose', data=pose_data)
+            pose_dset.attrs['description'] = "size (n,7) where 7 is (pos.x, pos.y, pos.z, quat.x, quat.y, quat.z, quat.w"
+
+            video_data = np.array(self.video)
+            video_dset = traj_group.create_dataset('video', data=video_data)
             self.reset()
 
-        elif key_pressed.data == 'o':
+        elif key_pressed == 'o':
             self.open_grasp()
             
-        elif key_pressed.data == 'c':
+        elif key_pressed == 'c':
             self.close_grasp()
+        elif key_pressed == 'h':
+            self.print_help()
+        elif key_pressed == 'x':
+            self.hd5f_file.close()
+            sys.exit(0)
         else:
-            print('Invalid key:', key_pressed.data)
+            print('Invalid key:', key_pressed)
 
+    def print_help(self):
+        print('h: Print help')
+        print('b: Begin data collection')
+        print('e: End data collection')
+        print('o: Open grasp')
+        print('c: Close grasp')
     def __del__(self):
         self.hd5f_file.close()
+
+def stdin_capture(callback):
+    while True:
+        print("Enter a key: ")
+        input_char = sys.stdin.readline().rstrip('\n')[0]
+        callback(input_char)
 
 if __name__ == '__main__':
     rospy.init_node("teleop_collection_node")
 	
     recorder = DataRecorder('test.h5')
-     
-    rospy.spin()
+    recorder.print_help()
 
+    video_thread = VideoCatureThread(recorder.cb_video_capture)
+    video_thread.start()
+
+    stdin_thread = threading.Thread(target=stdin_capture, args=(recorder.cb_keyboard,))
+    stdin_thread.start()
+    rospy.spin()
