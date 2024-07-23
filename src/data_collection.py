507ed2a05ceafc252bd5c16bc8bd79f7ba96f2ac
Dylan Miller <djm2@ualberta.ca>
2024-07-23 14:23:15 -0600

    Added rtsp stream and bhand data

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import threading
 import numpy as np
 import rospy 
 import h5py
 import time
 import cv2
+import argparse
+from pathlib import Path
 
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
 from wam_srvs.srv import JointMove
 from std_srvs.srv import Empty
+from bhand_teleop_msgs.msg import BhandTeleop
 
 class VideoCatureThread(threading.Thread):
-    def __init__(self, callback):
+    def __init__(self, callback, cap):
         threading.Thread.__init__(self)
         self.callback = callback
         self.running = False
+        self.cap = cap
 
     def run(self):
         self.running = True
-        pipe_line = "aravissrc camera-name=FLIR-0119E8A8 ! video/x-raw, framerate=30/1, format=RGB ! videoconvert ! appsink"
-        cap = cv2.VideoCapture(pipe_line, cv2.CAP_GSTREAMER)
         while self.running:
-            ret, frame = cap.read()
+            ret, frame = self.cap.read()
             if ret:
                 self.callback(frame)
             else:
                 print("Error: Could not read frame")
                 
-        cap.release()
+        self.cap.release()
 
     def stop(self):
         self.running = False
@@ -66,6 +69,10 @@ class JointStateData(DataInterface):
 
     def add(self, data):
         if self.check_collect_fn and self.check_collect_fn():
+            if len(data.velocity) == 0:
+                data.velocity = [np.nan]*len(data.position)
+            if len(data.effort) == 0:
+                data.effort = [np.nan]*len(data.position)
             joint_state = np.array([data.position, data.velocity, data.effort], dtype=float)
             self.joint_state_data.append(joint_state)
             self.time.append(rospy.get_time() - self.start_time_fn())
@@ -78,6 +85,30 @@ class JointStateData(DataInterface):
         joint_state_data = np.array(self.joint_state_data)
         joint_dset = hdf5_handle.create_dataset('joint_state', data=joint_state_data)
         joint_dset.attrs['description'] = "size = (n,3,dof) where 3 is (position, velocity, effort)" 
+        time_data = np.array(self.time)
+        time_dset = hdf5_handle.create_dataset('time', data=time_data)
+
+class HandVelCmdData(DataInterface):
+    def __init__(self, topic_name):
+        super().__init__()
+        self.cmd_vel_data = []
+        self.time = []
+        rospy.Subscriber(topic_name, BhandTeleop, self.add)
+
+    def add(self, data):
+        if self.check_collect_fn and self.check_collect_fn():
+            cmd_vel = np.array([data.spread, data.grasp], dtype=float)
+            self.cmd_vel_data.append(cmd_vel)
+            self.time.append(rospy.get_time() - self.start_time_fn())
+
+    def reset(self):
+        self.cmd_vel_data = []
+        self.time = []
+
+    def add_to_hdf5(self, hdf5_handle):
+        cmd_vel_data = np.array(self.cmd_vel_data)
+        joint_dset = hdf5_handle.create_dataset('cmd_vel', data=cmd_vel_data)
+        joint_dset.attrs['description'] = "size = (n,2) where 3 is (spread, grasp)" 
         time_data = np.array(self.time)
         time_dset = hdf5_handle.create_dataset('time', data=time_data)
 
@@ -107,11 +138,12 @@ class PoseData(DataInterface):
         time_dset = hdf5_handle.create_dataset('time', data=time_data)
 
 class VideoData(DataInterface):
-    def __init__(self):
+    def __init__(self, cap):
         super().__init__()
         self.video = []
         self.time = []
-        self.video_thread = VideoCatureThread(self.add)
+        
+        self.video_thread = VideoCatureThread(self.add, cap)
         self.video_thread.start()
 
     def add(self, data):
@@ -124,8 +156,22 @@ class VideoData(DataInterface):
         self.time = []
 
     def add_to_hdf5(self, hdf5_handle):
-        video_data = np.array(self.video)
-        video_dset = hdf5_handle.create_dataset('video', data=video_data, compression="gzip")
+        frame_paths = []
+        parent_path = Path(hdf5_handle.file.filename).parent
+        dir =  parent_path / hdf5_handle.name[1:]
+        dir.mkdir(parents=True, exist_ok=True)
+        rel_path = dir.relative_to(parent_path)
+        for i, frame in enumerate(self.video):
+            frame_filename = f"{i}.png"
+            frame_path_str = str(dir / frame_filename)
+            frame_relpath_str = str(rel_path / frame_filename)
+            print(frame_path_str)
+            frame_paths.append(frame_relpath_str)
+            cv2.imwrite(frame_path_str, frame, [cv2.IMWRITE_PNG_COMPRESSION, 8])
+
+        string_dt = h5py.string_dtype(encoding="utf-8")
+        path_dst = hdf5_handle.create_dataset('frame_paths', (len(frame_paths),), dtype=string_dt)
+        path_dst[:] = frame_paths
         time_data = np.array(self.time)
         time_dset = hdf5_handle.create_dataset('time', data=time_data)
 
@@ -135,12 +181,15 @@ class VideoData(DataInterface):
 
 class DataRecorder(object):
 
-    def __init__(self, save_file):
+    def __init__(self, save_path):
         self.open_grasp = rospy.ServiceProxy('/zeus/bhand/open_grasp', Empty) 
         self.close_grasp = rospy.ServiceProxy('/zeus/bhand/close_grasp', Empty)
-        self.save_file = save_file
+        self.save_file = save_path
 
-        self.hd5f_file = h5py.File(self.save_file, 'a')
+        dest_path = Path(save_path)
+        dest_path.mkdir(parents=True, exist_ok=True)
+
+        self.hd5f_file = h5py.File(str(dest_path / "data.h5"), 'a')
 
         self.collect = False
 
@@ -196,6 +245,7 @@ class DataRecorder(object):
             for name, data_type in self.datatypes.items():
                 data_group = traj_group.create_group(name)
                 data_type.add_to_hdf5(data_group)
+            print("saved")
             self.reset()
         elif key_pressed == 'o':
             self.open_grasp()
@@ -233,24 +283,28 @@ def stdin_capture(callback):
         callback(input_char)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save_path", type=str, required=True, help="Destination path of data")
+    args = parser.parse_args()
     rospy.init_node("teleop_collection_node")
-	
-    recorder = DataRecorder('test.h5')
+
+    recorder = DataRecorder(args.save_path)
     
-    leader_joint_state = JointStateData("leader/wam/joint_state")
-    recorder.add_datatype("leader/joint_state", leader_joint_state)
+    bhand_joint_state = JointStateData("/bhand/joint_states")
+    recorder.add_datatype("bhand/joint_state", bhand_joint_state)
 
-    leader_joint_pose = PoseData("leader/wam/pose")
-    recorder.add_datatype("leader/pose", leader_joint_pose)
-
-    follower_joint_state = JointStateData("follower/wam/joint_state")
+    follower_joint_state = JointStateData("/follower/joint_state")
     recorder.add_datatype("follower/joint_state", follower_joint_state)
 
-    follower_joint_pose = PoseData("follower/wam/pose")
-    recorder.add_datatype("follower/pose", follower_joint_pose)
+    hand_vel_cmd = HandVelCmdData("/bhand_mux/bhand_vel")
+    recorder.add_datatype("bhand/vel_cmd", hand_vel_cmd)
 
-    video_data = VideoData()
-    recorder.add_datatype("video", video_data)
+    # pipe_line = "aravissrc camera-name=FLIR-0119E8A8 ! video/x-raw, framerate=30/1, format=RGB ! videoconvert ! appsink"
+    # cap = cv2.VideoCapture(pipe_line, cv2.CAP_GSTREAMER)
+    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp|fflags;nobuffer|probesize;32|analyzeduration;0|max_delay;1"
+    vcap = cv2.VideoCapture("rtsp://192.168.1.11:8554/inhand", cv2.CAP_FFMPEG)
+    video_data = VideoData(vcap)
+    recorder.add_datatype("video_inhand", video_data)
 
     recorder.print_help()
 
